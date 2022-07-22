a3f1655a9b6103edbdde779cc33640742aa54191
Bladimir Minga <bsminga@pichincha.com>
2022-07-22 09:57:59 -0500

    Improvemnets are added

--- src/main/java/com/bp/cbe/configuration/ExceptionConfig.java
+++ src/main/java/com/bp/cbe/configuration/ExceptionConfig.java
@@ -19,67 +19,47 @@ import java.util.NoSuchElementException;
 @Slf4j
 @ControllerAdvice(annotations = RestController.class)
 public class ExceptionConfig {
-  @ExceptionHandler(NotFoundException.class)
-  public ResponseEntity<Object> notFoundException(Exception e) {
-    log.error(e.toString());
-    return buildResponseEntity(new ApiError(HttpStatus.NOT_FOUND, e.getMessage(), e));
-  }
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> notFoundException(Exception e) {
+        log.error(e.toString());
+        return buildResponseEntity(new ApiError(HttpStatus.NOT_FOUND, e.getMessage(), e));
+    }
 
-  @ExceptionHandler(NoSuchElementException.class)
-  public ResponseEntity<Object> noSuchElementException(NoSuchElementException e) {
-    log.error(e.toString());
-    return buildResponseEntity(new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e));
-  }
+    @ExceptionHandler({DataIntegrityViolationException.class, NoSuchElementException.class})
+    public ResponseEntity<Object> dataIntegrityViolationException(Exception e) {
+        log.error(e.toString());
+        return buildResponseEntity(new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e));
+    }
 
-  @ExceptionHandler(DataIntegrityViolationException.class)
-  public ResponseEntity<Object> dataIntegrityViolationException(DataIntegrityViolationException e) {
-    log.error(e.toString());
-    return buildResponseEntity(new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), e));
-  }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> constraintViolationException(ConstraintViolationException e) {
+        log.error(e.toString());
+        ApiError apiError =
+                new ApiError(HttpStatus.BAD_REQUEST, "An error occurred with parameter validation.", e);
+        e.getConstraintViolations()
+                .forEach(
+                        element ->
+                                apiError
+                                        .getSubErrors()
+                                        .add(new ApiSubError(element.getInvalidValue(), element.getMessage())));
+        return buildResponseEntity(apiError);
+    }
 
-  @ExceptionHandler(ConstraintViolationException.class)
-  public ResponseEntity<Object> constraintViolationException(ConstraintViolationException e) {
-    log.error(e.toString());
-    ApiError apiError =
-        new ApiError(HttpStatus.BAD_REQUEST, "An error occurred with parameter validation.", e);
-    e.getConstraintViolations()
-        .forEach(
-            element ->
-                apiError
-                    .getSubErrors()
-                    .add(new ApiSubError(element.getInvalidValue(), element.getMessage())));
-    return buildResponseEntity(apiError);
-  }
+    @ExceptionHandler({MethodArgumentNotValidException.class, BindException.class})
+    public ResponseEntity<Object> bindException(BindException e) {
+        log.error(e.toString());
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, "Error in DTO validation.", e);
+        e.getBindingResult()
+                .getAllErrors()
+                .forEach(
+                        element ->
+                                apiError
+                                        .getSubErrors()
+                                        .add(new ApiSubError(element.getArguments(), element.getDefaultMessage())));
+        return buildResponseEntity(apiError);
+    }
 
-  @ExceptionHandler(BindException.class)
-  public ResponseEntity<Object> bindException(BindException e) {
-    log.error(e.toString());
-    ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, "Error in DTO validation.", e);
-    e.getBindingResult()
-        .getAllErrors()
-        .forEach(
-            element ->
-                apiError
-                    .getSubErrors()
-                    .add(new ApiSubError(element.getArguments(), element.getDefaultMessage())));
-    return buildResponseEntity(apiError);
-  }
-
-  @ExceptionHandler({MethodArgumentNotValidException.class})
-  public ResponseEntity<Object> methodArgumentNotValidException(MethodArgumentNotValidException e) {
-    log.error(e.toString());
-    ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, "Error in DTO validation.", e);
-    e.getBindingResult()
-        .getAllErrors()
-        .forEach(
-            element ->
-                apiError
-                    .getSubErrors()
-                    .add(new ApiSubError(element.getArguments(), element.getDefaultMessage())));
-    return buildResponseEntity(apiError);
-  }
-
-  private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
-    return new ResponseEntity<>(apiError, apiError.getStatus());
-  }
+    private ResponseEntity<Object> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
 }
