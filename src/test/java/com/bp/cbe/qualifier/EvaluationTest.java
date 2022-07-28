cfcc5d97127f1bec08333680b8c8021c556c8a4d
Bladimir Minga <bsminga@pichincha.com>
2022-07-27 20:23:11 -0500

    DB H2 is added for integration tests.

--- src/test/java/com/bp/cbe/qualifier/EvaluationTest.java
+++ src/test/java/com/bp/cbe/qualifier/EvaluationTest.java
@@ -2,19 +2,21 @@ package com.bp.cbe.qualifier;
 
 import com.bp.cbe.domain.dto.OrganizationDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -22,9 +24,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase
+@ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 @AutoConfigureMockMvc
 class EvaluationTest {
 
@@ -35,8 +38,9 @@ class EvaluationTest {
     private ObjectMapper objectMapper;
 
     @Test
+    @DisplayName("create a organization")
     void createOrganizationShouldStoreCorrectly() throws Exception {
-        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post("/organizations").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(new OrganizationDto("Nueva")))).andExpect(status().isCreated()).andExpect(jsonPath("$.id").exists()).andExpect(jsonPath("$.name").exists()).andExpect(jsonPath("$.tribes").doesNotExist()).andReturn();
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post("/organizations").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(new OrganizationDto("Nueva")))).andExpect(status().isCreated()).andExpect(jsonPath("$.id", is(greaterThan(0)))).andExpect(jsonPath("$.name").exists()).andExpect(jsonPath("$.tribes").doesNotExist()).andReturn();
 
         OrganizationDto organizationDto = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), OrganizationDto.class);
 
@@ -58,10 +62,10 @@ class EvaluationTest {
     void getAllOrganizationsShouldReturnAll() throws Exception {
         mvc.perform(MockMvcRequestBuilders.post("/organizations").contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(new OrganizationDto("Todas")))).andExpect(status().isCreated()).andReturn();
 
-        ResultActions result = mvc.perform(MockMvcRequestBuilders.get("/organizations").contentType(MediaType.APPLICATION_JSON));
+        ResultActions result = mvc.perform(MockMvcRequestBuilders.get("/organizations").contentType(MediaType.APPLICATION_JSON)).andDo(print());
 
         result.andExpect(status().isOk());
-        result.andExpect(jsonPath("$.length()").value(2));
+        result.andExpect(jsonPath("$.length()", is(greaterThan(0))));
         result.andExpect(jsonPath("$[0].name").exists());
     }
 
---
+++ src/test/resources/data-h2.sql
@@ -0,0 +1,69 @@
+INSERT INTO organization (id_organization, name, status)
+VALUES (1, 'Organization Test', 1);
+
+INSERT INTO tribe (id_tribe, id_organization, name, status)
+VALUES (1, 1, 'Centro Digital', 1);
+INSERT INTO tribe (id_tribe, id_organization, name, status)
+VALUES (2, 1, 'Retail', 1);
+INSERT INTO tribe (id_tribe, id_organization, name, status)
+VALUES (3, 1, 'Canales', 1);
+INSERT INTO tribe (id_tribe, id_organization, name, status)
+VALUES (4, 1, 'Tecnología Informática', 1);
+
+INSERT INTO repository (id_repository, id_tribe, name, state, status, create_time)
+VALUES (1, 1, 'cd-common-utils', 'E', 1, '2022-11-01');
+INSERT INTO repository (id_repository, id_tribe, name, state, status, create_time)
+VALUES (2, 1, 'cd-common-text', 'E', 1, '2022-10-01');
+INSERT INTO repository (id_repository, id_tribe, name, state, status, create_time)
+VALUES (3, 1, 'cd-common-log', 'E', 1, '2022-09-01');
+INSERT INTO repository (id_repository, id_tribe, name, state, status, create_time)
+VALUES (4, 2, 'rtl-common-utils', 'E', 1, '2022-11-01');
+INSERT INTO repository (id_repository, id_tribe, name, state, status, create_time)
+VALUES (5, 2, 'rtl-common-text', 'E', 1, '2022-11-03');
+INSERT INTO repository (id_repository, id_tribe, name, state, status, create_time)
+VALUES (6, 2, 'rtl-common-log', 'E', 1, '2022-11-02');
+INSERT INTO repository (id_repository, id_tribe, name, state, status, create_time)
+VALUES (7, 2, 'rtl-common-dates', 'E', 1, '2022-11-02');
+INSERT INTO repository (id_repository, id_tribe, name, state, status, create_time)
+VALUES (8, 3, 'cnl-common-utils', 'E', 1, '2022-11-02');
+INSERT INTO repository (id_repository, id_tribe, name, state, status, create_time)
+VALUES (9, 3, 'cnl-common-text', 'E', 1, '2022-11-02');
+INSERT INTO repository (id_repository, id_tribe, name, state, status, create_time)
+VALUES (10, 3, 'cnl-common-log', 'E', 1, '2022-11-02');
+
+INSERT INTO metrics (id_metrics, coverage, bugs, vulnerabilities, hotspot, code_smells, id_repository)
+VALUES (1, 0.35, 11, 1, 0, 2, 1);
+INSERT INTO metrics (id_metrics, coverage, bugs, vulnerabilities, hotspot, code_smells, id_repository)
+VALUES (2, 0.25, 8, 0, 0, 0, 2);
+INSERT INTO metrics (id_metrics, coverage, bugs, vulnerabilities, hotspot, code_smells, id_repository)
+VALUES (3, 0.75, 0, 0, 0, 0, 3);
+INSERT INTO metrics (id_metrics, coverage, bugs, vulnerabilities, hotspot, code_smells, id_repository)
+VALUES (4, 0.93, 0, 0, 0, 0, 4);
+INSERT INTO metrics (id_metrics, coverage, bugs, vulnerabilities, hotspot, code_smells, id_repository)
+VALUES (5, 0.56, 5, 0, 1, 0, 5);
+INSERT INTO metrics (id_metrics, coverage, bugs, vulnerabilities, hotspot, code_smells, id_repository)
+VALUES (6, 0.80, 0, 1, 0, 3, 6);
+INSERT INTO metrics (id_metrics, coverage, bugs, vulnerabilities, hotspot, code_smells, id_repository)
+VALUES (7, 0.67, 2, 1, 0, 0, 7);
+INSERT INTO metrics (id_metrics, coverage, bugs, vulnerabilities, hotspot, code_smells, id_repository)
+VALUES (8, 0.96, 0, 0, 0, 0, 8);
+INSERT INTO metrics (id_metrics, coverage, bugs, vulnerabilities, hotspot, code_smells, id_repository)
+VALUES (9, 0.36, 2, 0, 1, 0, 9);
+INSERT INTO metrics (id_metrics, coverage, bugs, vulnerabilities, hotspot, code_smells, id_repository)
+VALUES (10, 0.39, 2, 0, 0, 2, 10);
+
+UPDATE GEN
+SET NEXT_VAL = 59
+WHERE SEQUENCE_NAME = 'metrics';
+
+UPDATE GEN
+SET NEXT_VAL = 50
+WHERE SEQUENCE_NAME = 'organization';
+
+UPDATE GEN
+SET NEXT_VAL = 53
+WHERE SEQUENCE_NAME = 'tribe';
+
+UPDATE GEN
+SET NEXT_VAL = 59
+WHERE SEQUENCE_NAME = 'repository';
