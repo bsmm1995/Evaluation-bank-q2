947a4c59c5a7227c62bbc94aa65849cf94994dd4
Bladimir Minga <bsminga@pichincha.com>
2022-08-26 13:20:37 -0500

    The code is refactored

--- src/main/java/com/bp/cbe/service/impl/ReportServiceImpl.java
+++ src/main/java/com/bp/cbe/service/impl/ReportServiceImpl.java
@@ -2,7 +2,6 @@ package com.bp.cbe.service.impl;
 
 import com.bp.cbe.domain.dto.RepositoryMetricsDto;
 import com.bp.cbe.domain.dto.webclient.RepositoryStatusDetail;
-import com.bp.cbe.domain.enums.VerificationCode;
 import com.bp.cbe.helpers.webclient.RepositoryStatusWC;
 import com.bp.cbe.repository.ReportRepository;
 import com.bp.cbe.service.ReportService;
@@ -12,9 +11,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 @Slf4j
@@ -23,24 +20,17 @@ public class ReportServiceImpl implements ReportService {
 
     private final ReportRepository repository;
     private final TribeService tribeService;
-
     private final RepositoryStatusWC repositoryStatus;
 
     @Override
     public List<RepositoryMetricsDto> getDataByTribe(long tribeId) {
         this.tribeService.getById(tribeId);
         List<RepositoryMetricsDto> result = repository.getReport(tribeId);
-        List<RepositoryStatusDetail> status = repositoryStatus.getAllStatus();
+        List<RepositoryStatusDetail> statusList = repositoryStatus.getAllStatus();
         result.forEach(e -> {
             e.setState(Status.getState(e.getState()));
-            e.setVerificationState(getVerificationState(e.getId(), status));
+            e.setVerificationState(Status.getVerificationState(e.getId(), statusList));
         });
         return result;
-    }
-
-    private String getVerificationState(long id, List<RepositoryStatusDetail> status) {
-        AtomicReference<String> verificationStatus = new AtomicReference<>("None");
-        status.stream().filter(s -> s.getId() == id).findFirst().flatMap(detail -> Arrays.stream(VerificationCode.values()).filter(e -> e.getCode() == detail.getState()).findFirst()).ifPresent(verificationCode -> verificationStatus.set(verificationCode.getDescription()));
-        return verificationStatus.get();
     }
 }
--- src/main/java/com/bp/cbe/utils/Status.java
+++ src/main/java/com/bp/cbe/utils/Status.java
@@ -1,5 +1,12 @@
 package com.bp.cbe.utils;
 
+import com.bp.cbe.domain.dto.webclient.RepositoryStatusDetail;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
 /**
  * Utility to map entities and data
  *
@@ -7,6 +14,8 @@ package com.bp.cbe.utils;
  * @version: 24/06/2022
  */
 public class Status {
+    private static final Map<Integer, String> statusMap = Map.of(604, "Verificado", 605, "En espera", 606, "Aprobado");
+
     private Status() {
         throw new IllegalStateException("Utility class");
     }
@@ -22,5 +31,12 @@ public class Status {
             default:
                 return "None";
         }
+    }
+
+    public static String getVerificationState(long id, List<RepositoryStatusDetail> status) {
+        AtomicReference<String> verificationStatus = new AtomicReference<>("");
+        Optional<RepositoryStatusDetail> optional = status.stream().filter(e -> e.getId() == id).findFirst();
+        optional.ifPresent(detail -> verificationStatus.set(statusMap.get((int) detail.getState())));
+        return verificationStatus.get();
     }
 }
